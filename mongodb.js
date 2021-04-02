78e68feff4b48ce3bf0884c7ff1416ca4425071f
wtwerner <wtwerner@gmail.com>
2021-04-02 10:51:58 -0500

    Create mongodb documents

--- mongodb.js
+++ mongodb.js
@@ -6,15 +6,59 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database')
     }
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Tommy',
-        age: 33
+    // db.collection('users').insertOne({
+    //     name: 'Tommy',
+    //     age: 33
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Lucy',
+    //         age: 33
+    //     },
+    //     {
+    //         name: 'Ella',
+    //         age: 2
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Do laundry',
+            completed: false
+        }, 
+        {
+            description: 'Order groceries',
+            completed: true
+        }, 
+        {
+            description: 'Give Max medicine',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert tasks')
+        }
+
+        console.log(result.ops)
     })
 })
