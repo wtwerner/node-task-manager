2ee00b319c8ee917f36cac7f64d547a67a006b7a
wtwerner <wtwerner@gmail.com>
2021-04-04 18:57:41 -0500

    Read documents from the database

--- mongodb.js
+++ mongodb.js
@@ -12,6 +12,24 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    
+    // db.collection('users').findOne({ _id: new ObjectID('60673b96c8b04612a14d3f88') }, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch')
+    //     }
+
+    //     console.log(user)
+    // })
+
+    // db.collection('users').find({ age: 33 }).toArray((error, users) => {
+    //     console.log(users)
+    // })
+
+    db.collection('tasks').findOne({ _id: new ObjectID('60673c7aad4b8b13f477055b')}, (error, task) => {
+        console.log(task)
+    })
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        console.log(tasks)
+    })
 })
 
