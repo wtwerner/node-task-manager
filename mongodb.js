f1fad1f2c9a809e0f4d555017c4954e56c0c378c
wtwerner <wtwerner@gmail.com>
2021-04-04 18:42:55 -0500

    Remove unused code

--- mongodb.js
+++ mongodb.js
@@ -5,9 +5,6 @@ const { MongoClient, ObjectID } = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id)
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database')
@@ -15,53 +12,6 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     name: 'Tommy',
-    //     age: 33
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert user')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Lucy',
-    //         age: 33
-    //     },
-    //     {
-    //         name: 'Ella',
-    //         age: 2
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert documents')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-//     db.collection('tasks').insertMany([
-//         {
-//             description: 'Do laundry',
-//             completed: false
-//         }, 
-//         {
-//             description: 'Order groceries',
-//             completed: true
-//         }, 
-//         {
-//             description: 'Give Max medicine',
-//             completed: false
-//         }
-//     ], (error, result) => {
-//         if (error) {
-//             return console.log('Unable to insert tasks')
-//         }
-
-//         console.log(result.ops)
-//     })
+    
 })
 
