0453ddc6f4f908805c20143ecddcb7a9839014ac
wtwerner <wtwerner@gmail.com>
2021-04-06 18:08:28 -0500

    Configure mongoose model validations

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -9,34 +10,63 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
     name: {
         type: String,
-        required: true
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
         type: Number,
+        default: 0,
         validate(value) {
             if (value < 0) {
                 throw new Error('Age must be a positive number')
             }
         }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password must not comtain "password"')
+            }
+        }
     }
 })
 
-const me = new User({
+// const me = new User({
+//     name: '   Tommy  ',
+//     email: 'MYEMAIL@test.com  ',
+//     password: "phone098!" 
+// })
 
-})
-
-me.save().then((me) => {
-    console.log(me)
-}).catch((err) => {
-    console.log('Error!', err)
-})
+// me.save().then((me) => {
+//     console.log(me)
+// }).catch((err) => {
+//     console.log('Error!', err)
+// })
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     }, 
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
