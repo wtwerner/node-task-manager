ee192d1b6c05c42e88406dd75837d350698daa55
wtwerner <wtwerner@gmail.com>
2021-04-08 15:14:40 -0500

    Create endpoints for users and tasks

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+// Users Endpoints
+
 app.post('/users', (req, res) => {
     const user = new User(req.body)
 
@@ -18,6 +20,30 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+// Tasks Endpoints
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
@@ -27,6 +53,30 @@ app.post('/tasks', (req, res) => {
         res.status(400).send(e)
     })
 })
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+// Start Server
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
