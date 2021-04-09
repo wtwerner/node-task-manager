2245259da767df9765576785309554a7c334a6d3
wtwerner <wtwerner@gmail.com>
2021-04-09 16:24:55 -0500

    Refactor update routes and hash passwords

--- src/index.js
+++ src/index.js
@@ -13,3 +13,20 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+    const password = 'red12345!'
+    const hashedPassword = await bcrypt.hash('Red12345!', 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare(password, hashedPassword)
+    
+    console.log(isMatch)
+}
+
+myFunction()
