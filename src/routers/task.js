6bc4cf145b00a97171fefc405554ac4ad08e42dc
wtwerner <wtwerner@gmail.com>
2021-04-13 13:01:10 -0500

    Allow for filtering of tasks via endpoint URL

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,10 +17,21 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+// GET /tasks?completed={true/false}
+
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
     try {
-        const tasks = await Task.find({ owner: req.user._id })
-        res.send(tasks)
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate()
+        res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
     }
