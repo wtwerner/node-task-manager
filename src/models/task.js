b202507f0630be1aaf642ad2f7cb23ac403d9a63
wtwerner <wtwerner@gmail.com>
2021-04-13 11:42:09 -0500

    Define User/Task relationship

--- src/models/task.js
+++ src/models/task.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 const Task = mongoose.model('Task', {
     description: {
@@ -10,6 +9,11 @@ const Task = mongoose.model('Task', {
     completed: {
         type: Boolean,
         default: false
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     }
 })
 
