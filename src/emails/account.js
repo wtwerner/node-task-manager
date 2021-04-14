c55d9307a475661f7ea84886864aa03efd1b3359
wtwerner <wtwerner@gmail.com>
2021-04-13 20:40:30 -0500

    Change from address for emails

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'wtwerner@gmail.com',
+        from: 'tommy@wtwerner.dev',
         subject: 'Welcome to Task App',
         text: `Welcome to Task App, ${name}! Let me know how you like the app.`
     })
@@ -14,7 +14,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'wtwerner@gmail.com',
+        from: 'tommy@wtwerner.dev',
         subject: 'Sorry to see you go!',
         text: `You have sucessfully canceled your Task App account, ${name}. Is there anything we could have done better?`
     })
