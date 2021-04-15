597ee14dafdd9ba358aa2c89473c01a1ccef852a
wtwerner <wtwerner@gmail.com>
2021-04-15 12:12:11 -0500

    Add login and logout testing

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,10 +1,36 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: 'Abcd1234!'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
         name: 'Tommy',
         email: 'tommy@example.com',
-        password: 'abcd1234!'
+        password: 'Abcd1234!'
     }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should not login with nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email: 'baduser@example.com',
+        password: 'Abcd1234!'
+    }).expect(400)
 })
