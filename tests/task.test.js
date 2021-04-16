bbc166a049c6f53e886592d0019086025fa58f24
wtwerner <wtwerner@gmail.com>
2021-04-16 12:45:08 -0500

    Add test to delete other user tasks

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,7 +1,16 @@
 const request = require('supertest')
 const app = require('../src/app')
 const Task = require('../src/models/task')
-const { userOne, userOneId, setupDatabase } = require('./fixtures/db')
+const { 
+    userOne, 
+    userOneId, 
+    userTwo, 
+    userTwoId,
+    taskOne,
+    taskTwo,
+    taskThree,
+    setupDatabase 
+} = require('./fixtures/db')
 
 beforeEach(setupDatabase)
 
@@ -17,4 +26,25 @@ test('Should create task for user', async () => {
     const task = await Task.findById(response.body._id)
     expect(task).not.toBeNull()
     expect(task.completed).toBe(false)
+})
+
+test('Should get tasks for user', async () => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    expect(response.body.length).toEqual(2)
+})
+
+test('Should not delete other users tasks', async () => {
+    const response = await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404)
+
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
 })
